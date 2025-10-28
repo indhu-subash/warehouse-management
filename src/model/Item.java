fe8298669157a00961e505b6d45eefff63f7bb21
INDHU SUBASH <indhusubash2028@cs.sjcetpalai.ac.in>
2025-10-28 14:33:09 +0530

    Add new fields and constructors to Item class

--- src/model/Item.java
+++ src/model/Item.java
@@ -9,6 +9,13 @@ public class Item {
     private String name;
     private int quantity;
     private String location;
+    private String category;
+    private String description;
+    private int minStockLevel;
+    private double price;
+    private int supplierId;
+    private String createdDate;
+    private String updatedDate;
     
     /**
      * Default constructor
@@ -21,12 +28,23 @@ public class Item {
      * @param name Item name
      * @param quantity Item quantity
      * @param location Item location in warehouse
+     * @param category Item category
+     * @param description Item description
+     * @param minStockLevel Minimum stock level
+     * @param price Item price
+     * @param supplierId Supplier ID
      */
-    public Item(int id, String name, int quantity, String location) {
+    public Item(int id, String name, int quantity, String location, String category, 
+                String description, int minStockLevel, double price, int supplierId) {
         this.id = id;
         this.name = name;
         this.quantity = quantity;
         this.location = location;
+        this.category = category;
+        this.description = description;
+        this.minStockLevel = minStockLevel;
+        this.price = price;
+        this.supplierId = supplierId;
     }
     
     /**
@@ -34,11 +52,22 @@ public class Item {
      * @param name Item name
      * @param quantity Item quantity
      * @param location Item location in warehouse
+     * @param category Item category
+     * @param description Item description
+     * @param minStockLevel Minimum stock level
+     * @param price Item price
+     * @param supplierId Supplier ID
      */
-    public Item(String name, int quantity, String location) {
+    public Item(String name, int quantity, String location, String category, 
+                String description, int minStockLevel, double price, int supplierId) {
         this.name = name;
         this.quantity = quantity;
         this.location = location;
+        this.category = category;
+        this.description = description;
+        this.minStockLevel = minStockLevel;
+        this.price = price;
+        this.supplierId = supplierId;
     }
     
     // Getters and Setters
@@ -74,6 +103,70 @@ public class Item {
         this.location = location;
     }
     
+    public String getCategory() {
+        return category;
+    }
+    
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public int getMinStockLevel() {
+        return minStockLevel;
+    }
+    
+    public void setMinStockLevel(int minStockLevel) {
+        this.minStockLevel = minStockLevel;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+    
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+    public int getSupplierId() {
+        return supplierId;
+    }
+    
+    public void setSupplierId(int supplierId) {
+        this.supplierId = supplierId;
+    }
+    
+    public String getCreatedDate() {
+        return createdDate;
+    }
+    
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+    
+    public String getUpdatedDate() {
+        return updatedDate;
+    }
+    
+    public void setUpdatedDate(String updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+    
+    /**
+     * Check if item is low in stock
+     * @return true if quantity is below minimum stock level
+     */
+    public boolean isLowStock() {
+        return quantity <= minStockLevel;
+    }
+    
     /**
      * String representation of Item
      * @return formatted string with item details
@@ -85,6 +178,8 @@ public class Item {
                 ", name='" + name + '\'' +
                 ", quantity=" + quantity +
                 ", location='" + location + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
                 '}';
     }
 }
